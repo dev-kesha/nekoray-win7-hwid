585ea30119952583e5d9f2933716303064e051af
Nikita <38914329+dev-kesha@users.noreply.github.com>
2026-02-12 22:02:47 +0300

    Update GroupUpdater.cpp

--- src/configs/sub/GroupUpdater.cpp
+++ src/configs/sub/GroupUpdater.cpp
@@ -430,7 +430,7 @@ namespace Subscription {
             auto groupName = group == nullptr ? content : group->name;
             MW_show_log(">>>>>>>> " + QObject::tr("Requesting subscription: %1").arg(groupName));
 
-            auto resp = NetworkRequestHelper::HttpGet(content, Configs::dataManager->settingsRepo->sub_send_hwid);
+            uto resp = NetworkRequestHelper::HttpGet(content, true);
             if (!resp.error.isEmpty()) {
                 MW_show_log("<<<<<<<< " + QObject::tr("Requesting subscription %1 error: %2").arg(groupName, resp.error + "\n" + resp.data));
                 return;
